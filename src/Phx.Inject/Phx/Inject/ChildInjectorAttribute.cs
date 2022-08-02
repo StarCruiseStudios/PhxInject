3dc8f95be369c5b5616530a7e65994addb1e91df
Dangerdan9631 <dangerdan9631@live.com>
2022-08-01 18:10:47 -0700

    Add child injector attributes.

---
+++ src/Phx.Inject/Phx/Inject/ChildInjectorAttribute.cs
@@ -0,0 +1,17 @@
+﻿// -----------------------------------------------------------------------------
+//  <copyright file="ChildInjectorAttribute.cs" company="Star Cruise Studios LLC">
+//      Copyright (c) 2022 Star Cruise Studios LLC. All rights reserved.
+//      Licensed under the Apache License 2.0 License.
+//      See http://www.apache.org/licenses/LICENSE-2.0 for full license information.
+//  </copyright>
+// -----------------------------------------------------------------------------
+
+namespace Phx.Inject {
+    using System;
+
+    /// <summary>
+    ///     Annotates a method used to retrieve a child injector instance.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Method)]
+    public class ChildInjectorAttribute : Attribute { }
+}
---
+++ src/Phx.Inject/Phx/Inject/ExternalDependencyAttribute.cs
@@ -0,0 +1,30 @@
+﻿// -----------------------------------------------------------------------------
+//  <copyright file="ExternalDependencyAttribute.cs" company="Star Cruise Studios LLC">
+//      Copyright (c) 2022 Star Cruise Studios LLC. All rights reserved.
+//      Licensed under the Apache License 2.0 License.
+//      See http://www.apache.org/licenses/LICENSE-2.0 for full license information.
+//  </copyright>
+// -----------------------------------------------------------------------------
+
+namespace Phx.Inject {
+    using System;
+
+    /// <summary>
+    ///     Defines an external dependency that is required by an injector interface.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Interface)]
+    public class ExternalDependencyAttribute : Attribute {
+        /// <summary>
+        ///     Gets the type of the external dependency.
+        /// </summary>
+        public Type ExternalDependency { get; }
+
+        /// <summary>
+        ///     Initialzes a new instance of the <see cref="ExternalDependencyAttribute"/> class.
+        /// </summary>
+        /// <param name="externalDependency"> The type of the external dependency. </param>
+        public ExternalDependencyAttribute(Type externalDependency) {
+            ExternalDependency = externalDependency;
+        }
+    }
+}
