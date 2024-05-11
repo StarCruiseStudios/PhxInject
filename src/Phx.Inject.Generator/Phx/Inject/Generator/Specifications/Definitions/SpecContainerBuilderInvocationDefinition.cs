ee6db7b065a68ca2d9efae3e6dc49ef2327a2684
Dangerdan9631 <dangerdan9631@live.com>
2024-05-10 19:41:34 -0700

    Clean up SpecContainerBuilderInvocationDefinition.cs

--- src/Phx.Inject.Generator/Phx/Inject/Generator/Specifications/Definitions/SpecContainerBuilderInvocationDefinition.cs
+++ src/Phx.Inject.Generator/Phx/Inject/Generator/Specifications/Definitions/SpecContainerBuilderInvocationDefinition.cs
@@ -11,33 +11,9 @@ namespace Phx.Inject.Generator.Specifications.Definitions {
     using Phx.Inject.Generator.Common;
     using Phx.Inject.Generator.Common.Definitions;
 
-    // internal delegate SpecContainerBuilderInvocationDefinition CreateSpecContainerBuilderInvocationDefinition(
-    //         InjectorDescriptor injectorDescriptor,
-    //         BuilderRegistration builderRegistration,
-    //         Location location);
-
     internal record SpecContainerBuilderInvocationDefinition(
             TypeModel SpecContainerType,
             string BuilderMethodName,
             Location Location
-    ) : IDefinition {
-        // public class Builder {
-        //     private readonly CreateSpecContainerReferenceDefinition createSpecContainerReference;
-        //
-        //     public Builder(CreateSpecContainerReferenceDefinition createSpecContainerReference) {
-        //         this.createSpecContainerReference = createSpecContainerReference;
-        //     }
-        //
-        //     public SpecContainerBuilderInvocationDefinition Build(
-        //             InjectorDescriptor injectorDescriptor,
-        //             BuilderRegistration builderRegistration,
-        //             Location location
-        //     ) {
-        //         return new SpecContainerBuilderInvocationDefinition(
-        //                 createSpecContainerReference(injectorDescriptor, builderRegistration.Specification),
-        //                 builderRegistration.BuilderDescriptor.BuilderMethodName,
-        //                 location);
-        //     }
-        // }
-    }
+    ) : IDefinition;
 }
